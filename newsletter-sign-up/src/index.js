1e960192458eba1deb3ed304664e6e34c30363a8
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-14 21:09:32 +0100

    Import and use EmailProvider

--- newsletter-sign-up/src/index.js
+++ newsletter-sign-up/src/index.js
@@ -2,10 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { EmailProvider } from "./context/emailContext";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/success",
+    element: <h1>Success</h1>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <EmailProvider>
+      <RouterProvider router={router} />
+    </EmailProvider>
   </React.StrictMode>
 );
